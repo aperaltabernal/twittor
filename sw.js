b61f44a781eaa19f7b28a7d7f9d228444e5944c5
joan <andres.hardattack@hgmail.com>
2022-08-29 23:31:17 -0500

    Modificaciones a iphone X para quitar notch

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //importaciones: ESTAS IMPORTACIONES DEBEN AGREGARSE TAMBIEN EN EL APP_SHELL
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v4';
-const DYNAMIC_CACHE = 'dynamic-v1'
+const STATIC_CACHE = 'static-v5';
+const DYNAMIC_CACHE = 'dynamic-v2'
 const INMUTABLE_CACHE = 'inmutable-v1';//GUARDAMOS LAS LIBRERIAS DE TERCEROS
 
 const APP_SHELL = [
@@ -40,6 +40,9 @@ self.addEventListener('activate', event => {
     const activation = caches.keys().then( keys => {
         keys.forEach( key => {
             if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
                 return caches.delete(key);
             }
         });
